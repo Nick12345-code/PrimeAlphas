e311c5cc83f794ac851f987533c80d02be3f9d68
El-Parto <luc980@y7mail.com>
2021-05-21 15:24:45 +1000

    Ai intergrated in level

---
+++ Assets/Scripts/AI/Notes/EnemyNotes.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyNotes
+{
+    /*
+     * This object is the AI that moves around to different waypoints based upon the class
+     * " EnemyWaypoints
+     * 
+     * 
+     * 
+     * 
+     * */
+ 
+
+}
