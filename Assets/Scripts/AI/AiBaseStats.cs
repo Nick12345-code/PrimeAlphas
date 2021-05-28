5bbfabef8fe9ba3fdc28541c8a8d57319b0acdd2
El-Parto <luc980@y7mail.com>
2021-05-28 16:25:44 +1000

    Ai now looks at turrets

--- Assets/Scripts/AI/AiBaseStats.cs
+++ Assets/Scripts/AI/AiBaseStats.cs
@@ -13,6 +13,13 @@ namespace AIBehaviour
 
         public float attack = 2f; // base attack variable for future use
 
+
+
+        //public List<string>   = new List<string>();
+
+        [SerializeField]
+        //private string ;
+
         
         //public NavMeshAgent navAgentSettings; // this may be used to cahnge the AI speed, acceleration in the future
         //public Collider turretCollisions;
@@ -32,15 +39,19 @@ namespace AIBehaviour
 
         private void OnTriggerStay(Collider triggerColl)
         {
-            
+            if (triggerColl.gameObject.CompareTag("Turret"))
+            {
+                //triggerColl.gameObject.SetActive(false);
+
+                gameObject.transform.LookAt(triggerColl.transform);
+
+            }
+
         }
 
         private void OnTriggerEnter(Collider triggerColl)
         {
-            if (triggerColl.gameObject.CompareTag("Turret"))
-            {
-                triggerColl.gameObject.SetActive(false);
-            }
+
         }
 
 
