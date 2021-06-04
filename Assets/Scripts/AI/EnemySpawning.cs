a11754adcb89c1640cb52792d17a7b70c5e88015
El-Parto <luc980@y7mail.com>
2021-06-04 16:33:56 +1000

    basic buggy spawner done

--- Assets/Scripts/AI/EnemySpawning.cs
+++ Assets/Scripts/AI/EnemySpawning.cs
@@ -6,13 +6,23 @@ public class EnemySpawning : MonoBehaviour
 {
 
     public GameObject enemy;
+    public GameObject enemyOpp;
 
-    //public int[] enemyCount;
-    public List<EnemySpawning> enemyCount;
+    public GameObject spawnPos;
+
+    public int enemyCount = 1; // how many enemies are spawned.
+    public int spawnRate = 10; // the number of seconds it will take for an enemy to spawn
+    [SerializeField]
+    private int spawnCase;
+    //public List<EnemySpawning> enemyCount;
 
 
-    //public int[] waveNumber;
+    public int waveNumber = 1; // the current number of the wave
+    public int waveLevel = 1; // difficulty of wave?
+    public float waveTimer = 100; // how much time the current wave has (integer)
 
+
+    private bool waveActive = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,10 +34,61 @@ public class EnemySpawning : MonoBehaviour
     {
         
     }
+    /// <summary>
+    /// needs spawn rate how many enemies that spawn in a wave during the associated 
+    /// timer of said wave.
+    /// 
+    /// psudo code
+    /// 
+    /// first wave;
+    /// set wave to 1,
+    /// start reducing the timer by time.deltatime,
+    /// wave one has one enemy spawning every 10 seconds.
+    /// 
+    /// </summary>
+    /// <param name="_enemyCount"></param>
+    /// <returns></returns>
+    
 
-    ////public IEnumerator Spawner()
+    public void OnClick()
+    {
+        StartCoroutine(EnemyWaveSpawner());
+    }
+    
+    public IEnumerator EnemyWaveSpawner()
+    {
+        while (waveTimer > 0)
+        {
+            spawnCase = Random.Range(1, 2);
+            waveTimer -= waveTimer - 1 * Time.deltaTime;
+            switch (spawnCase)
+            {
+                case 1:
+                    Instantiate(enemy, spawnPos.transform.position, Quaternion.identity);
+                    yield return new WaitForSeconds(spawnRate);
+                    break;
+                case 2:
+                    Instantiate(enemyOpp, spawnPos.transform.position, Quaternion.identity);
+                    yield return new WaitForSeconds(spawnRate);
+                    break;
+            }
+        }
+        //if (waveActive)
+        //{
+            
+        //}        
+    }
+
+    //public void WaveControl(int _waveNumber)
     //{
-        
-    //}
+    //    `
 
+    //    /// waves
+    //    waveNumber = _waveNumber;
+    //    if (waveActive)
+    //    {
+    //        StartCoroutine(EnemyWaveSpawner());
+    //    }
+
+    //}
 }
